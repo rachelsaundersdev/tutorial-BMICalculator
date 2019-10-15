9b59d4bf2dfd199665afd9d1da6a3f70f0107fa6
Rachel Saunders <rachelsaundersBA@gmail.com>
2019-10-15 15:24:07 +0100

    Initial Commit

---
+++ BMI Calculator/Controllers/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Rachel Saunders on 15/10/2019.
+//  Copyright © 2019 Rachel Saunders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        
+    }
+    
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        
+    }
+    
+}
+
