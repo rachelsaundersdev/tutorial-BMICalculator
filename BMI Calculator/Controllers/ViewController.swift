c292a56214d51d744f4395d3bffba76b839fa128
Rachel Saunders <rachelsaundersBA@gmail.com>
2019-10-15 15:34:29 +0100

    Updated labels to reflect the slider value.

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,11 +21,14 @@ class ViewController: UIViewController {
 
 
     @IBAction func heightSliderChanged(_ sender: UISlider) {
-        
+       let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
     }
     
     
     @IBAction func weightSliderChanged(_ sender: UISlider) {
+       let weight = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(weight)Kg"
         
     }
     
